96e71fc2c3c9a211566482fbd83bc422e77066f1
Kevin PM <no@example.com>
2021-03-23 21:16:29 -0400

    p5

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -8,8 +8,8 @@ export default function Entete(props) {
             <img src="images-projets/Projet06.gif" alt="" className="logo"/>
             <ul className="navPrincipale">
                 <li><a href="#Apropos">À Propos</a></li>
-                <li><a href="ListeProjets">Projets</a></li>
-                <li><a href="Contacts">Contact</a></li>
+                <li><a href="#ListeProjets">Projets</a></li>
+                <li><a href="#Contacts">Contact</a></li>
             </ul>
         </header>
     );
